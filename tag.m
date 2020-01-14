c1c2b32f67ef49c1d4f2ac3696f58f4c67470866
dante3861 <32917219+dante3861@users.noreply.github.com>
2020-01-14 14:48:33 +0000

    Add files via upload
    to set a .txt file including good/bad markers for algorithm to read

---
+++ tag.m
@@ -0,0 +1,47 @@
+% cd means present file path
+% output also in this path
+% change value of my_type if you want to change the file type
+%-------------------------------------------------------------------------%
+%-------------------------------------------------------------------------%
+my_type    = '*.jpg';
+%-------------------------------------------------------------------------%
+fileFolder = fullfile(cd,'pretest_good');               
+dirOutput  = dir(fullfile(fileFolder,my_type)); 
+fileNames  = {dirOutput.name}';                 
+fileNames  = char(fileNames);
+[line,row] = size(fileNames);                   
+NewNames   = zeros(line,row+2);                 
+pre_line=line;
+for i=1:line
+    NewNames(i,:)=[fileNames(i,:) ' 1'];        
+end
+NewNames   = char(NewNames);                   
+disp(NewNames);                               
+
+save_path  = cd;                               
+file_name  = 'TEST.txt';                        
+target     = fullfile(save_path,file_name);
+fid        = fopen(target,'w+');                
+for i=1:line
+    fprintf(fid,[NewNames(i,:) '\r\n']);        
+end
+fileFolder = fullfile(cd,'pretest_bad');                
+dirOutput  = dir(fullfile(fileFolder,my_type)); 
+fileNames  = {dirOutput.name}';                 
+fileNames  = char(fileNames);
+[line,row] = size(fileNames);                   
+NewNames   = zeros(line,row+2);
+for i=1:line
+    NewNames(i,:)=[fileNames(i,:) ' 2'];
+end
+NewNames   = char(NewNames);
+disp(NewNames);
+for i=1:line
+    fprintf(fid,[NewNames(i,:) '\r\n']);
+end
+fclose(fid);
+
+
+
+
+
